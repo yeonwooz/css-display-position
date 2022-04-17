8da7f6f1a944a1c4164561562c63ec19487b0382
suyeon woo <woosean999@gmail.com>
2022-04-17 12:28:47 +0900

    box 네개 생성

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='box div-1'></div>
+      <div className='box div-2'></div>
+      <p className='box p-1'></p>
+      <p className='box p-2'></p>
+
     </div>
   );
 }
